a7637ca95374be93d7c22063314014179f4af1e7
paulo <paulo_vicali@icloud.com>
2024-06-28 17:39:15 -0300

    feat(main): setup autenticação JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,25 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLinks } from './routes/send-auth-links'
 import chalk from 'chalk'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
 const port = 3333
-const app = new Elysia().use(registerRestaurant).use(sendAuthLinks)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLinks)
 
 app.listen(port, () => {
   console.log(chalk.redBright(`🚀 Server running on port ${port}`))
